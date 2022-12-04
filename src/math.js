8f89bbed551c94663c06757525230468c09eabd4
Intiser Rajoan Parash <irparash@gmail.com>
2022-12-04 01:01:37 -0600

    First Commit

---
+++ src/math.js
@@ -0,0 +1,6 @@
+export default constants = {
+  gravitationalConstant: 6.67408 * Math.pow(10, -11),
+  earthSunDistanceMeters: 1.496 * Math.pow(10, 11),
+  earthAngularVelocityMetersPerSecond: 1.990986 * Math.pow(10, -7),
+  massOfTheSunKg: 1.98855 * Math.pow(10, 30)
+}
